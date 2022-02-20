f3f5283dad6d7cae6ea95ba40d990994c40d9e69
Nick <kuznaik@gmail.com>
2022-02-20 11:34:05 -0600

    typo

--- src/components/Widget/InvertAxis/InvertAxis.js
+++ src/components/Widget/InvertAxis/InvertAxis.js
@@ -4,18 +4,18 @@ const InvertAxisToggle = ({ labelX, labelY, unitsX, unitsY, toggleInvertAxis }) 
             <div>
                 <div>
                     <span>X-axis: </span>
-                    <span className="value">
+                    <span>
                         {labelX}, {unitsX}
                     </span>
                 </div>
                 <div>
                     <span>Y-axis: </span>
-                    <span className="value">
+                    <span>
                         {labelY}, {unitsY}
                     </span>
                 </div>
             </div>
-            <button onClick={toggleInvertAxis}>Toggle Axis</button>
+            <button onClick={toggleInvertAxis}>Invert Axis</button>
         </>
     );
 };
